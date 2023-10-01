13de9278bd7c34bf4ecf81863f263d72cb4d2643
George <georgecocks01@gmail.com>
2023-10-02 00:29:46 +0100

    Coded game, fixed errors

---
+++ hangman.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+  string words[6] = {"programming", "computer", "cheese", "balloon", "criminal", "friend"};
+  string hangmanArt[7] = {
+    " +---+\n"
+    " |   |\n"
+    "     |\n"
+    "     |\n"
+    "     |\n"
+    "     |\n"
+    "==========",
+    " +---+\n"
+    " |   |\n"
+    " O   |\n"
+    "     |\n"
+    "     |\n"
+    "     |\n"
+    "==========",
+    " +---+\n"
+    " |   |\n"
+    " O   |\n"
+    " |   |\n"
+    "     |\n"
+    "     |\n"
+    "==========",
+    " +---+\n"
+    " |   |\n"
+    " O   |\n"
+    "/|   |\n"
+    "     |\n"
+    "     |\n"
+    "==========",
+    " +---+\n"
+    " |   |\n"
+    " O   |\n"
+    "/|\\  |\n"
+    "     |\n"
+    "     |\n"
+    "==========",
+    " +---+\n"
+    " |   |\n"
+    " O   |\n"
+    "/|\\  |\n"
+    "/    |\n"
+    "     |\n"
+    "==========",
+    " +---+\n"
+    " |   |\n"
+    " O   |\n"
+    "/|\\  |\n"
+    "/ \\  |\n"
+    "     |\n"
+    "=========="
+};
+
+  srand(time(NULL));
+  int ranNum = rand();
+  int arraySize = (sizeof(words)/sizeof(string));
+  int ranWordNum = ranNum % arraySize;
+
+  string word = words[ranWordNum];
+
+  // Make the placeholder for letters guessed
+  string guessingWord;
+  for (int i = 0; i < word.size(); i++) {
+    guessingWord.append("_");
+  }
+
+  bool win = false;
+  bool lose = false;
+  int turn = 0;
+  int oldTurn = -1;
+  char guessedLetter;
+  vector<char> usedLetters;
+
+  cout << "Welcome to hangman!" << endl;
+  while (win == false && lose == false) { // Game loop
+    if (turn > oldTurn) {
+      cout << hangmanArt[turn] << endl;
+    }
+    cout << endl << "Word: " << guessingWord << " (" << guessingWord.size() << ") letters" << endl;
+
+    if (oldTurn != -1) { // Print used letters
+      cout << "(Used letters:";
+      for (char letter : usedLetters) {
+        cout << " " << letter;
+      }
+      cout << ")";
+    }
+    cout << endl << "Enter letter to guess: ";
+    cin >> guessedLetter;
+
+    int position = word.find(guessedLetter);
+
+    if (position != string::npos) {
+      cout << guessedLetter << " is in the word!" << endl;
+      while (position < word.size() && position != string::npos) {
+        guessingWord[position] = guessedLetter;
+        position = word.find(guessedLetter, position + 1);
+      }
+      oldTurn = turn;
+    } else {
+      cout << guessedLetter << " isn't in the word." << endl;
+      oldTurn = turn;
+      turn++;
+    }
+
+    if (guessingWord.find("_") == string::npos) {
+      win = true;
+    } else if (turn == sizeof(hangmanArt)/sizeof(hangmanArt[0])) {
+      lose = true;
+    }
+
+    usedLetters.push_back(guessedLetter);
+  }
+
+  cout << endl << "The word was " << word << endl;
+
+  return 0;
+}
